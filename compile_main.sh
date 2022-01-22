496e68b67bee3969dbe422573c0a6b5e647940b0
Markus  Rudolph <markhoernchen@gmx.de>
2022-01-22 20:52:45 +0100

    Add a setup

--- compile_main.sh
+++ compile_main.sh
@@ -3,7 +3,7 @@ rm -rf bin
 mkdir -p bin bin/src
 
 echo "Assembling database..."
-cpp src/main.sql | grep -wv ^# | grep -v -e '^\s*$' > bin/src/main.sql
+cpp src/main.sql | sed s/__NL/\\r\\n/g | grep -wv ^# | grep -v -e '^\s*$' > bin/src/main.sql
 
 echo "Compiling database..."
 sqlite3 bin/src/main.sqlite < bin/src/main.sql
--- src/main.sql
+++ src/main.sql
@@ -1,6 +1,15 @@
-#include "tables.hsql"
-#include "columns.hsql"
-#include "values.hsql"
-#include "changes.hsql"
-#include "actions.hsql"
-#include "categories.hsql"
+#include "undo_log/library.hsql"
+
+CREATE TABLE persons (
+  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL
+);
+
+__SETUP_UNDO_LOG(persons);
+
+__BEGIN_ACTION(add)
+INSERT INTO persons (name) VALUES ('Markus');
+INSERT INTO persons (name) VALUES ('Jenny');
+INSERT INTO persons (name) VALUES ('Rebecca');
+INSERT INTO persons (name) VALUES ('Dorothea');
+__END_ACTION
