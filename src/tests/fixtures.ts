c9b13d3ec24b54dbf9831c1020b1a81025527146
Markus  Rudolph <markhoernchen@gmx.de>
2022-02-14 21:53:44 +0100

    Force removal of output

--- src/tests/fixtures.ts
+++ src/tests/fixtures.ts
@@ -9,62 +9,70 @@ import { UndoLogUtilsImpl } from "../impl/undo-log-utils";
 import { SqliteType, TableDefinition } from "../tables";
 
 export namespace AllTypeTable {
-    export const Definition: TableDefinition = {
-        name: "all_types",
-        primaryKey: ["id"],
-        columns: {
-            id: "INTEGER",
-            name: "TEXT",
-            num: "NUMERIC",
-            blob: "BLOB",
-            zero: "REAL"
-        }        
-    };
-    export interface Row {
-        id: number;
-        name: string;
-        num: number;
-        blob: Buffer;
-        zero: number;
-    }
-    export const Values: Row[] = [
-        {
-            id: 1,
-            blob: Buffer.from("juan"),
-            name: "one",
-            num: 1,
-            zero: 0
-        }
-    ]
+  export const Definition: TableDefinition = {
+    name: "all_types",
+    primaryKey: ["id"],
+    columns: {
+      id: "INTEGER",
+      name: "TEXT",
+      num: "NUMERIC",
+      blob: "BLOB",
+      zero: "REAL",
+    },
+  };
+  export interface Row {
+    id: number;
+    name: string;
+    num: number;
+    blob: Buffer;
+    zero: number;
+  }
+  export const Values: Row[] = [
+    {
+      id: 1,
+      blob: Buffer.from("juan"),
+      name: "one",
+      num: 1,
+      zero: 0,
+    },
+  ];
 }
 
 export async function setupBeforeAll() {
-  await promises.rm("output", {recursive: true});
+  await promises.rm("output", {
+    recursive: true,
+    force: true,
+  });
   await promises.mkdir("output");
 }
 
 export async function setupBeforeEach() {
-    const fileName = path.join("output", expect.getState().currentTestName.replace(/\s+/g, "_") + ".sqlite3");
-    const database: Database = new DatabaseImpl(fileName);
-    const connection = await database.connect();
-    const utils = new UndoLogUtilsImpl(connection)
-    const setup = new UndoLogSetupImpl(connection, utils);
-    const log = new UndoLogImpl(connection, utils);
-    const assertions = new UndoLogAssertionsImpl(utils);
-    return {
-        connection,
-        utils,
-        setup,
-        log,
-        assertions
-    };
+  const fileName = path.join(
+    "output",
+    expect.getState().currentTestName.replace(/\s+/g, "_") + ".sqlite3"
+  );
+  const database: Database = new DatabaseImpl(fileName);
+  const connection = await database.connect();
+  const utils = new UndoLogUtilsImpl(connection);
+  const setup = new UndoLogSetupImpl(connection, utils);
+  const log = new UndoLogImpl(connection, utils);
+  const assertions = new UndoLogAssertionsImpl(utils);
+  return {
+    connection,
+    utils,
+    setup,
+    log,
+    assertions,
+  };
 }
 
-export const TableDefintion_OnlyOneType: (type: SqliteType) => TableDefinition = (type: SqliteType) => ({
-    name: `${type}_only`,
-    primaryKey: ["id"],
-    columns: {
-        id: "INTEGER",
-        col: type
-    }        
+export const TableDefintion_OnlyOneType: (
+  type: SqliteType
+) => TableDefinition = (type: SqliteType) => ({
+  name: `${type}_only`,
+  primaryKey: ["id"],
+  columns: {
+    id: "INTEGER",
+    col: type,
+  },
 });
