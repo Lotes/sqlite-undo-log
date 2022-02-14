0a26cfcca65892c3dd3fdf822deadef436a78040
Markus  Rudolph <markhoernchen@gmx.de>
2022-02-14 21:29:12 +0100

    Setup sonarcloud

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testResultsProcessor: "jest-sonar-reporter",
   maxWorkers: 1,
+  collectCoverage: true,
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+    "/src/tests/"
+  ],
   maxConcurrency: 1,
   globalSetup: "./src/tests/jestGlobalSetup.js"
 };
